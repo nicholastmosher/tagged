db2ed59afd398609cb76b1291f4f5aaa22cbf9f9
Nick Mosher <nicholastmosher@pm.me>
2026-03-01 04:27:13 -0500

    experimenting with popovers w/profile bar

--- crates/tagged/src/components/profile_switcher.rs
+++ crates/tagged/src/components/profile_switcher.rs
@@ -1,5 +1,6 @@
 use std::path::PathBuf;
 
+use tracing::info;
 use zed::unstable::{
     component,
     gpui::{
@@ -8,9 +9,10 @@ use zed::unstable::{
     },
     ui::{
         ActiveTheme, AnyElement, App, AudioStatus, Avatar, AvatarAudioStatusIndicator,
-        ButtonCommon, ButtonLike, ButtonSize, ButtonStyle, Clickable, Component, Context, Element,
-        ElementId, FluentBuilder as _, IconButton, IconName, IconSize, InteractiveElement,
-        IntoElement, ParentElement as _, PopoverMenu, RegisterComponent, Rems, Render, RenderOnce,
+        AvatarAvailabilityIndicator, ButtonCommon, ButtonLike, ButtonSize, ButtonStyle, Clickable,
+        CollaboratorAvailability, Component, Context, Disableable, Element, ElementId,
+        FluentBuilder as _, IconButton, IconName, IconSize, InteractiveElement, IntoElement,
+        ParentElement as _, Popover, PopoverMenu, RegisterComponent, Rems, Render, RenderOnce,
         SharedString, StatefulInteractiveElement as _, Styled, Toggleable, Tooltip, Window, div,
         h_flex, px, v_flex,
     },
@@ -83,35 +85,6 @@ impl ProfileBar {
 
 impl Render for ProfileBar {
     fn render(&mut self, _window: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
-        let popover = PopoverMenu::new("editor-settings")
-            .trigger({
-                //
-                ProfileNugget::new("profile-nugget-myself", self.active_profile.clone()).on_click({
-                    let profile = self.active_profile.clone();
-                    move |_e, _window, cx| {
-                        profile.update(cx, |profile, cx| {
-                            profile.online = !profile.online;
-                            cx.notify();
-                        })
-                    }
-                })
-            })
-            // .trigger_with_tooltip(
-            //     IconButton::new("toggle_editor_settings_icon", IconName::Sliders)
-            //         .icon_size(IconSize::Custom(Rems(1.)))
-            //         .size(ButtonSize::Large)
-            //         .style(ButtonStyle::Subtle),
-            //     // .toggle_state(self.toggle_settings_handle.is_deployed()),
-            //     Tooltip::text("Editor Controls"),
-            // )
-            .anchor(Corner::TopRight)
-            // .with_handle(self.toggle_settings_handle.clone())
-            .menu(move |window, cx| {
-                //
-                let switcher = cx.new(|cx| ProfileSwitcher::new(cx));
-                Some(switcher)
-            });
-
         h_flex()
             //
             .child(
@@ -126,7 +99,17 @@ impl Render for ProfileBar {
                     .child(
                         // Profile icon, name, and status
                         // self.render_profile_icon_name_status(window, cx),
-                        popover,
+                        ProfileNugget::new("profile-nugget-myself", self.active_profile.clone())
+                            .on_click({
+                                let profile = self.active_profile.clone();
+                                move |_e, _window, cx| {
+                                    info!("Clicked profile nugget 1");
+                                    profile.update(cx, |profile, cx| {
+                                        profile.online = !profile.online;
+                                        cx.notify();
+                                    })
+                                }
+                            }),
                     )
                     .child(
                         h_flex()
@@ -134,12 +117,12 @@ impl Render for ProfileBar {
                             .gap_4()
                             .child(
                                 IconButton::new("profile-mute", IconName::Mic)
-                                    .icon_size(IconSize::Custom(Rems(1.)))
+                                    .icon_size(IconSize::Custom(Rems(1.25)))
                                     .size(ButtonSize::Large),
                             )
                             .child(
                                 IconButton::new("profile-deafen", IconName::AudioOn)
-                                    .icon_size(IconSize::Custom(Rems(1.)))
+                                    .icon_size(IconSize::Custom(Rems(1.25)))
                                     .size(ButtonSize::Large),
                             ), // .child(popover),
                     ),
@@ -153,13 +136,12 @@ impl Component for ProfileBar {
     fn preview(_window: &mut Window, cx: &mut App) -> Option<AnyElement> {
         let active_profile =
             cx.new(|cx| Profile::new("Myselfandi", cx).with_avatar(".assets/tagged.svg"));
-        let hud = cx.new(|cx| ProfileBar::new("the-profile", active_profile, cx));
+        let profile_bar = cx.new(|cx| ProfileBar::new("the-profile", active_profile, cx));
         let canvas = div()
             //
             .debug()
             .p_4()
-            // .child(ProfileHud::new(active_profile.clone()));
-            .child(hud);
+            .child(profile_bar);
         Some(Element::into_any(canvas))
     }
 }
@@ -213,6 +195,45 @@ impl ProfileNugget {
     }
 }
 
+impl ButtonCommon for ProfileNugget {
+    fn id(&self) -> &ElementId {
+        self.base.id()
+    }
+
+    fn style(mut self, style: ButtonStyle) -> Self {
+        self.base = self.base.style(style);
+        self
+    }
+
+    fn size(mut self, size: ButtonSize) -> Self {
+        self.base = self.base.size(size);
+        self
+    }
+
+    fn tooltip(
+        mut self,
+        tooltip: impl Fn(&mut Window, &mut App) -> gpui::AnyView + 'static,
+    ) -> Self {
+        self.base = self.base.tooltip(tooltip);
+        self
+    }
+
+    fn tab_index(mut self, tab_index: impl Into<isize>) -> Self {
+        self.base = self.base.tab_index(tab_index);
+        self
+    }
+
+    fn layer(mut self, elevation: zed::unstable::ui::ElevationIndex) -> Self {
+        self.base = self.base.layer(elevation);
+        self
+    }
+
+    fn track_focus(mut self, focus_handle: &FocusHandle) -> Self {
+        self.base = self.base.track_focus(focus_handle);
+        self
+    }
+}
+
 impl Clickable for ProfileNugget {
     fn on_click(mut self, handler: impl Fn(&ClickEvent, &mut Window, &mut App) + 'static) -> Self {
         self.base = self.base.on_click(handler);
@@ -225,6 +246,13 @@ impl Clickable for ProfileNugget {
     }
 }
 
+impl Disableable for ProfileNugget {
+    fn disabled(mut self, disabled: bool) -> Self {
+        self.base = self.base.disabled(disabled);
+        self
+    }
+}
+
 impl Toggleable for ProfileNugget {
     fn toggle_state(mut self, selected: bool) -> Self {
         self.base = self.base.toggle_state(selected);
@@ -233,10 +261,11 @@ impl Toggleable for ProfileNugget {
 }
 
 impl RenderOnce for ProfileNugget {
-    fn render(self, window: &mut Window, cx: &mut App) -> impl IntoElement {
+    fn render(self, _window: &mut Window, cx: &mut App) -> impl IntoElement {
         let active_bg_color = cx.theme().colors().ghost_element_active;
         let hover_bg_color = cx.theme().colors().ghost_element_hover;
-        let hover_border_color = cx.theme().colors().border.opacity(1.0);
+        let _hover_border_color = cx.theme().colors().border.opacity(1.0);
+        let profile = self.profile.clone();
 
         h_flex()
             //
@@ -246,6 +275,13 @@ impl RenderOnce for ProfileNugget {
             .active(|style| style.bg(active_bg_color))
             .hover(|style| style.bg(hover_bg_color))
             .rounded_md()
+            .on_click(move |_e, _window, cx| {
+                info!("Clicked profile nugget 2");
+                profile.update(cx, |profile, cx| {
+                    profile.online = !profile.online;
+                    cx.notify();
+                });
+            })
             .when_some(self.profile.read(cx).avatar.as_ref(), |it, avatar| {
                 //
                 it.child(
@@ -255,12 +291,13 @@ impl RenderOnce for ProfileNugget {
                             Avatar::new(avatar.clone())
                                 //
                                 .size(px(40.))
-                                .indicator(
-                                    AvatarAudioStatusIndicator::new(AudioStatus::Muted).tooltip(
-                                        //
-                                        Tooltip::text("tagged profile"),
-                                    ),
-                                ),
+                                .indicator(AvatarAvailabilityIndicator::new(
+                                    if self.profile.read(cx).online {
+                                        CollaboratorAvailability::Free
+                                    } else {
+                                        CollaboratorAvailability::Busy
+                                    },
+                                )),
                         ),
                 )
                 //
