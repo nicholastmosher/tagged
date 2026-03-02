1f64d46ac7a54169c14c29bda602d5888998b503
Nick Mosher <nicholastmosher@pm.me>
2026-03-01 21:52:41 -0500

    make ProfileBar and ProfileNugget RenderOnce

--- crates/tagged/src/components/mod.rs
+++ crates/tagged/src/components/mod.rs
@@ -1,11 +1,11 @@
 use zed::unstable::ui::App;
 
-pub mod profile_switcher;
+pub mod profile_bar;
 pub mod space_icon;
 pub mod tagged_panel;
 
 pub fn init(cx: &mut App) {
-    profile_switcher::init(cx);
+    profile_bar::init(cx);
     tagged_panel::init(cx);
 
     // cx.observe_new(|workspace: &mut Workspace, window, cx| {
